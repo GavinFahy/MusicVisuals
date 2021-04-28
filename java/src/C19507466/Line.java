3e83c5eafd00184028d97844a3e7b1668646f61b
Gavin Fahy <gavinf37@gmail.com>
2021-04-28 21:53:15 +0100

    i have changed how i wish for my lines to be dislayed, instead of a wave form i want a bouncing line

--- java/src/C19507466/Line.java
+++ java/src/C19507466/Line.java
@@ -32,14 +32,14 @@ public class Line {
         for(int i = 0 ; i < gv.getAudioBuffer().size() ; i ++)
         {
             gv.stroke(PApplet.map(i, 0, gv.getAudioBuffer().size(), 0, 255), 255, 255);
-            gv.line(699, 699, 699,50 * gv.getAudioBuffer().get(i));
+            gv.line(699, 699 * gv.getAudioBuffer().get(i), 699,50);
         }
 
         //bottom line
         for(int i = 0 ; i < gv.getAudioBuffer().size() ; i ++)
         {
             gv.stroke(PApplet.map(i, 0, gv.getAudioBuffer().size(), 0, 255), 255, 255);
-            gv.line(699, 699, 1,699);
+            gv.line(699 * gv.getAudioBuffer().get(i), 699, 1 * gv.getAudioBuffer().get(i),699);
         }
     }
 }
